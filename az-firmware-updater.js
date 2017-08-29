f9370db220117f6aee792b1151c03c1b9914e4d0
James Sturtevant <jsturtevant@gmail.com>
2017-08-29 14:15:31 -0400

    refactored reporting logic

--- az-firmware-updater.js
+++ az-firmware-updater.js
@@ -1,9 +1,10 @@
 const url = require('url');
 const downloader = require('./lib/downloader');
+const HubReporter = require('./lib/hub-reporter')
 
 module.exports = class FirmwareUpdater {
     constructor(client, options) {
-        this.client = client;
+        this.hubReporter = new HubReporter(client);
         this.options = options;
     }
 
@@ -21,7 +22,7 @@ module.exports = class FirmwareUpdater {
     initiateFirmwareUpdateFlow(fwPackageUri, callback) {
         console.log("firmware upgrade started")
 
-        this.resetFirmwareReport()
+        this.hubReporter.resetFirmwareReport()
             .then(_ => this.download(fwPackageUri))
             .then(fileLocation => this.apply(fileLocation))
             .then(_ => this.restart())
@@ -31,7 +32,7 @@ module.exports = class FirmwareUpdater {
             })
             .catch(err => {
                 console.log(err);
-                this.sendErrorReport(err).then(_ => {
+                this.hubReporter.sendErrorReport(err).then(_ => {
                     console.log("error reported.")
                 }).catch(err => {
                     // should retry?
@@ -49,111 +50,39 @@ module.exports = class FirmwareUpdater {
         }
         
         if (typeof this.options.restart !== 'function') {
-            return Promise.reject('must be function');
+            return Promise.reject('restart option must be function');
         }
 
         console.log("restarting...");
         const self = this;
-        this.sendRestartReport().then(_ => self.options.restart());
-    }
-
-    resetFirmwareReport() {
-        // setting to null clears in reported property.
-        return this.reportFWUpdateThroughTwin(null);
+        this.hubReporter.sendRestartReport().then(_ => self.options.restart());
     }
 
     download(fwPackageUri) {
-        return this.sendDownloadingReport()
-            .then(_ => downloader.download(fwPackageUri, this.options.downloadOpts))
-            .then(fileLocation => {
-                this.sendDownloadedReport().then(_ => {
-                    return Promise.resolve(fileLocation);
+        return this.hubReporter.sendDownloadingReport()
+                .then(_ => downloader.download(fwPackageUri, this.options.downloadOpts))
+                .then(fileLocation => {
+                    this.hubReporter.sendDownloadedReport().then(_ => {
+                        return Promise.resolve(fileLocation);
+                    });
                 });
-            });
-    }
-
-    sendDownloadingReport() {
-        return this.reportFWUpdateThroughTwin({
-            status: 'downloading',
-            startedDownloadingTime: new Date().toISOString()
-        })
-    }
-
-    sendDownloadedReport() {
-        return this.reportFWUpdateThroughTwin({
-            status: 'download complete',
-            downloadCompleteTime: new Date().toISOString()
-        })
     }
 
     apply(fileLocation) {
-        return this.sendApplyingReport()
-            .then(_ => this.applyImage(fileLocation))
-            .then(_ => this.sendAppliedReport())
-    }
-
-    sendApplyingReport() {
-        return this.reportFWUpdateThroughTwin({
-            status: 'applying',
-            startedApplyingImage: new Date().toISOString()
-        });
+        return this.hubReporter.sendApplyingReport()
+                .then(_ => this.applyImage(fileLocation))
+                .then(_ => this.hubReporter.sendAppliedReport())
     }
 
     applyImage(fileLocation) {
         if (!this.options.applyImage || typeof this.options.applyImage !== 'function') {
-            return Promise.reject('must be function');
+            return Promise.reject('applyImage option must be function');
         }
 
         return this.options.applyImage(fileLocation);
     }
 
-    sendAppliedReport() {
-        return this.reportFWUpdateThroughTwin({
-            status: 'apply complete',
-            lastFirmwareUpdate: new Date().toISOString()
-        });
-    }
-
-    sendRestartReport() {
-        return this.reportFWUpdateThroughTwin({
-            status: 'restarting',
-            startedRestart: new Date().toISOString()
-        });
-    }
- 
-
-    sendErrorReport(err) {
-        return this.reportFWUpdateThroughTwin({
-            status: 'error',
-            errorMessage: err,
-            errorTime: new Date().toISOString()
-        });
-    }
-
-    reportFWUpdateThroughTwin(firmwareUpdateValue) {
-        const self = this;
-        const patch = {
-            iothubDM: {
-                firmwareUpdate: firmwareUpdateValue
-            }
-        };
-
-        return new Promise(function (fulfill, reject) {
-            self.client.getTwin(function (err, twin) {
-                if (err) {
-                    return reject(err);
-                }
-
-                twin.properties.reported.update(patch, function (reportingErr) {
-                    if (reportingErr) {
-                        return reject(reportingErr);
-                    }
-
-                    return fulfill();
-                });
-            });
-        });
-    }
+    
 
 }
 
